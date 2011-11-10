d3d1d8a21b890a5cfa62273c5e1dd2faaa33b85d
James Kozianski <koz@chromium.org>
2011-11-11 08:16:57 +1100

    Add a test suite for all unit tests.

---
+++ test/com/id/test/AllTests.java
@@ -0,0 +1,20 @@
+package com.id.test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.id.editor.EditorTest;
+import com.id.editor.RangeTest;
+import com.id.file.FileTest;
+import com.id.file.FileViewTest;
+import com.id.file.GraveyardTest;
+import com.id.fuzzy.FuzzyFinderTest;
+import com.id.platform.InMemoryFileSystemTest;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ EditorTest.class, RangeTest.class, FileTest.class,
+    FileViewTest.class, GraveyardTest.class, FuzzyFinderTest.class,
+    InMemoryFileSystemTest.class })
+public class AllTests {
+
+}

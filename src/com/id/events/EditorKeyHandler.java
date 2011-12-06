8d978c164ceb75487156da973a8539647a64e307
James Kozianski <koz@chromium.org>
2011-12-05 18:08:45 -0800

    Use more ShortcutTree.

--- src/com/id/events/EditorKeyHandler.java
+++ src/com/id/events/EditorKeyHandler.java
@@ -5,46 +5,47 @@ import java.util.Arrays;
 
 import com.id.editor.Editor;
 import com.id.editor.Visual;
-import com.id.events.ShortcutTree.Action;
 
 public class EditorKeyHandler {
   private final ShortcutTree normalTree;
+  private final ShortcutTree visualTree;
   private Editor editor;
 
   public EditorKeyHandler() {
     normalTree = new ShortcutTree();
+    visualTree = new ShortcutTree();
 
-    setNormal("j", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("j"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.down();
       }
     });
-    setNormal("k", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("k"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.up();
       }
     });
-    setNormal("h", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("h"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.left();
       }
     });
-    setNormal("l", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("l"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.right();
       }
     });
-    setNormal("w", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("w"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.moveCursorToNextWord();
       }
     });
-    setNormal("i", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("i"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         // TODO This should be handled at a higher level - we shouldn't be
@@ -55,182 +56,191 @@ public class EditorKeyHandler {
         editor.insert();
       }
     });
-    setNormal("s", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("s"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.substitute();
       }
     });
-    setNormal("u", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("u"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.undo();
       }
     });
-    setNormal("r", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("r"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.redo();
       }
     });
-    setNormal("o", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("o"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.addEmptyLine();
       }
     });
-    setNormal("a", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("a"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.append();
       }
     });
-    setNormal("v", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("v"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.toggleVisual(Visual.Mode.CHAR);
       }
     });
-    setNormal("x", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("x"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.delete();
       }
     });
-    setNormal("0", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("0"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.moveCursorToStartOfLine();
       }
     });
-    setNormal(KeyStroke.backspace(), new ShortcutTree.Action() {
+    normalTree.setShortcut(Arrays.asList(KeyStroke.backspace()), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.backspace();
       }
     });
-    setNormal("\\", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("\\"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.clearHighlight();
       }
     });
-    setNormal("z", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("z"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.recenter();
       }
     });
-    setNormal("n", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("n"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.next();
       }
     });
-    setNormal(KeyStroke.escape(), new ShortcutTree.Action() {
+    normalTree.setShortcut(Arrays.asList(KeyStroke.escape()), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.escape();
       }
     });
-    setNormal("gg", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("gg"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.moveCursorToStartOfFile();
       }
     });
-    setNormal("cc", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("cc"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.substituteLine();
       }
     });
-    setNormal("O", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("O"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.addEmptyLinePrevious();
       }
     });
-    setNormal("A", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("A"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.appendEnd();
       }
     });
-    setNormal("I", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("I"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.insertStart();
       }
     });
-    setNormal("V", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("V"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.toggleVisual(Visual.Mode.LINE);
       }
     });
-    setNormal("C", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("C"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.changeLine();
       }
     });
-    setNormal("D", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("D"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.deleteLine();
       }
     });
-    setNormal("$", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("$"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.moveCursorToEndOfLine();
       }
     });
-    setNormal("S", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("S"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.substituteLine();
       }
     });
-    setNormal("*", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("*"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.highlightWordUnderCursor();
       }
     });
-    setNormal("N", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("N"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.previous();
       }
     });
-    setNormal("G", new ShortcutTree.Action() {
+    normalTree.setShortcut(KeyStroke.fromString("G"), new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.moveCursorToEndOfFile();
       }
     });
-    setNormal(KeyStroke.fromControlChar('f'), new ShortcutTree.Action() {
+    normalTree.setShortcut(Arrays.asList(KeyStroke.fromControlChar('f')),
+        new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.downPage();
       }
     });
-    setNormal(KeyStroke.fromControlChar('b'), new ShortcutTree.Action() {
+    normalTree.setShortcut(Arrays.asList(KeyStroke.fromControlChar('b')),
+        new ShortcutTree.Action() {
       @Override
       public void execute() {
         editor.upPage();
       }
     });
-  }
 
-  private void setNormal(KeyStroke key, Action action) {
-    normalTree.setShortcut(Arrays.asList(key), action);
-  }
-
-  private void setNormal(String string, Action action) {
-    normalTree.setShortcut(KeyStroke.fromString(string), action);
+    visualTree.setShortcut(KeyStroke.fromString("J"),
+        new ShortcutTree.Action() {
+      @Override
+      public void execute() {
+        editor.join();
+      }
+    });
+    visualTree.setShortcut(KeyStroke.fromString("K"),
+        new ShortcutTree.Action() {
+      @Override
+      public void execute() {
+        // Do nothing.
+      }
+    });
   }
 
   public boolean handleKeyPress(KeyStroke event, Editor editor) {
@@ -250,29 +260,8 @@ public class EditorKeyHandler {
       }
     }
 
-    if (editor.isInVisual()) {
-      if (event.isShiftDown()) {
-        boolean handled = true;
-        switch (event.getKeyCode()) {
-        case KeyEvent.VK_J:
-          editor.join();
-          break;
-        case KeyEvent.VK_K:
-          // Do nothing.
-          break;
-        default:
-          handled = false;
-          break;
-        }
-        if (handled) {
-          return true;
-        }
-      } else {
-        this.editor = editor;
-        return normalTree.stepAndExecute(event);
-      }
-    }
     this.editor = editor;
-    return normalTree.stepAndExecute(event);
+    return (editor.isInVisual() && visualTree.stepAndExecute(event))
+        || normalTree.stepAndExecute(event);
   }
 }

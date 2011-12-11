bbf93c9bc92a88ad9a9de9869bf3d4dfe14266ca
James Kozianski <koz@chromium.org>
2011-12-10 16:57:57 -0800

    Make EditorKeyHandlerTest extend EditorTestBase.

--- test/com/id/events/EditorKeyHandlerTest.java
+++ test/com/id/events/EditorKeyHandlerTest.java
@@ -5,26 +5,16 @@ import static org.junit.Assert.assertFalse;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.id.editor.Editor;
-import com.id.file.File;
-import com.id.file.FileView;
+import com.id.test.EditorTestBase;
 
-public class EditorKeyHandlerTest {
-  private File file;
-  private FileView fileView;
-  private Editor editor;
-  private EditorKeyHandler handler;
-
+public class EditorKeyHandlerTest extends EditorTestBase {
   @Before
   public void setup() {
-    file = new File();
-    fileView = new FileView(file);
-    editor = new Editor(fileView);
-    handler = new EditorKeyHandler();
+    setFileContents();
   }
 
   @Test
-  public void testHandling() {
+  public void unhandledKeystrokesReturnFalse() {
     // NOTE This will break if we ever add a key binding for q in the editor.
     assertFalse(handler.handleKeyPress(KeyStroke.fromChar('q'), editor));
   }

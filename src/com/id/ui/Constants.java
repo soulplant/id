08188229861a21ac55b3178832bb1258021ec2e0
James Kozianski <koz@chromium.org>
2012-06-29 18:42:07 +1000

    Make the highlight color lighter.

--- src/com/id/ui/Constants.java
+++ src/com/id/ui/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
   public static final Color MARKER_ADDED_COLOR = new Color(141, 255, 126);
   public static final Color MARKER_MODIFIED_COLOR = Color.ORANGE;
   public static final Color MARKER_REMOVED_COLOR = Color.RED;
-  public static final Color HIGHLIGHT_COLOR = new Color(104, 147, 255);
+  public static final Color HIGHLIGHT_COLOR = new Color(166, 192, 255);
   public static final Color WHITESPACE_INDICATOR_COLOR = Color.GRAY;
   public static final Color EIGHTY_CHAR_INDICATOR_COLOR = Color.GRAY;
   public static final Color TEXT_COLOR = Color.BLACK;

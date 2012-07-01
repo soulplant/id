01d8b637d7e766b1089404d12890cb6cf698e0d7
James Kozianski <koz@chromium.org>
2012-07-01 10:32:37 +1000

    Add a line underneath the editor title.

--- src/com/id/ui/editor/EditorTitleView.java
+++ src/com/id/ui/editor/EditorTitleView.java
@@ -7,10 +7,13 @@ import java.awt.Graphics;
 import com.id.app.App;
 import com.id.app.ListModel;
 import com.id.editor.Editor;
+import com.id.ui.Constants;
 import com.id.ui.app.LinewisePanel;
 
 @SuppressWarnings("serial")
 public class EditorTitleView extends LinewisePanel {
+  private static final int BOTTOM_PADDING_PX = 13;
+  private static final int LINE_PADDING_RIGHT_PX = 10;
   private final Editor editor;
   private final ListModel<Editor> editors;
 
@@ -25,11 +28,14 @@ public class EditorTitleView extends LinewisePanel {
     App.configureFont(g);
     g.setColor(isFocused() ? Color.BLACK: Color.GRAY);
     g.drawString(getTitle(), 0, getFontHeightPx() - 3);
+    g.setColor(Constants.TITLE_LINE_COLOR);
+    int lineY = getFontHeightPx() + 5;
+    g.drawLine(0, lineY, getWidth() - LINE_PADDING_RIGHT_PX, lineY);
   }
 
   @Override
   public Dimension getPreferredSize() {
-    return new Dimension(getTitle().length() * getFontWidthPx(), getFontHeightPx());
+    return new Dimension(getTitle().length() * getFontWidthPx(), getFontHeightPx() + BOTTOM_PADDING_PX);
   }
 
   private boolean isFocused() {

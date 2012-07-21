b4ca168a0dc1fe429e16351fc80f25306a8a0ebe
James Kozianski <soulplant@gmail.com>
2012-07-21 13:16:13 +1000

    Rename variables inside SpotlightView to be more generic.

--- src/com/id/ui/app/SpotlightView.java
+++ src/com/id/ui/app/SpotlightView.java
@@ -11,42 +11,42 @@ import com.id.app.ListModel;
 @SuppressWarnings("serial")
 public class SpotlightView<M, V extends JPanel> extends JPanel implements ListModel.Listener<M> {
   private final Map<M, V> map = new HashMap<M, V>();
-  private final ListModel<M> editors;
+  private final ListModel<M> models;
   private final LinewisePanel placeholder = new LinewisePanel();
   private final ViewFactory<M, V> viewFactory;
 
-  public SpotlightView(ListModel<M> editors, ViewFactory<M, V> viewFactory) {
-    this.editors = editors;
+  public SpotlightView(ListModel<M> models, ViewFactory<M, V> viewFactory) {
+    this.models = models;
     this.viewFactory = viewFactory;
     setLayout(new BorderLayout());
-    for (int i = 0; i < editors.size(); i++) {
-      M editor = editors.get(i);
-      V editorPanel = viewFactory.createView(editor);
-      map.put(editor, editorPanel);
+    for (int i = 0; i < models.size(); i++) {
+      M model = models.get(i);
+      V view = viewFactory.createView(model);
+      map.put(model, view);
     }
     refresh();
   }
 
   private void refresh() {
     super.removeAll();
-    if (editors.isEmpty()) {
+    if (models.isEmpty()) {
       super.add(placeholder);
       return;
     }
-    super.add(map.get(editors.getFocusedItem()));
+    super.add(map.get(models.getFocusedItem()));
   }
 
   @Override
-  public void onAdded(int i, M editor) {
+  public void onAdded(int i, M model) {
     // TODO(koz): This is some hax here. We don't remove old editors, in case they are
     // being added and removed straight away. This currently happens when ctrl-j/k is
     // typed. We don't want to throw away the EditorPanel because it holds state such
     // as the scroll (which is also a design flaw). What should happen is we should
     // send move events as well as add/remove, so that we don't leak EditorPanels.
-    V editorPanel = map.get(editor);
-    if (editorPanel == null) {
-      editorPanel = viewFactory.createView(editor);
-      map.put(editor, editorPanel);
+    V view = map.get(model);
+    if (view == null) {
+      view = viewFactory.createView(model);
+      map.put(model, view);
     }
     refresh();
   }

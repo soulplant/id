596701a214e83d0dca40b5a905d157e857a4a31f
James Kozianski <soulplant@gmail.com>
2012-08-24 07:50:32 +1000

    Enable anti-aliasing only apply for text.

--- src/com/id/app/App.java
+++ src/com/id/app/App.java
@@ -139,7 +139,7 @@ public class App {
     // the text even when it's off anyway.
     if (!isMacOS()) {
       Graphics2D g2 = (Graphics2D) g;
-      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
     }
   }
 

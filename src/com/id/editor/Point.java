a8b964a4ce590d7bfebe666daf437288757dc13a
James Kozianski <soulplant@gmail.com>
2012-09-12 13:20:46 +1000

    Define Point.hashCode().

--- src/com/id/editor/Point.java
+++ src/com/id/editor/Point.java
@@ -35,12 +35,28 @@ public class Point {
   }
 
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + x;
+    result = prime * result + y;
+    return result;
+  }
+
+  @Override
   public boolean equals(Object obj) {
-    if (!(obj instanceof Point)) {
+    if (this == obj)
+      return true;
+    if (obj == null)
       return false;
-    }
+    if (getClass() != obj.getClass())
+      return false;
     Point other = (Point) obj;
-    return this.y == other.y && this.x == other.x;
+    if (x != other.x)
+      return false;
+    if (y != other.y)
+      return false;
+    return true;
   }
 
   public Point offset(int dy, int dx) {

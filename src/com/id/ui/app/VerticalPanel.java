849e817dcb327773dfe176602e2760b0bc68da29
James Kozianski <koz@chromium.org>
2012-09-22 06:01:46 +1000

    Remove incorrect and unnecessary VerticalPanel.getPreferredSize().

--- src/com/id/ui/app/VerticalPanel.java
+++ src/com/id/ui/app/VerticalPanel.java
@@ -13,16 +13,4 @@ public class VerticalPanel extends JPanel {
     setLayout(new VerticalListLayout(verticalPaddingPx, 0));
     setBackground(Constants.BG_COLOR);
   }
-
-  @Override
-  public Dimension getPreferredSize() {
-    int width = 0;
-    int height = 0;
-    for (int i = 0; i < getComponentCount(); i++) {
-      Dimension preferredSize = getComponent(i).getPreferredSize();
-      width = Math.max(width, preferredSize.width);
-      height += preferredSize.height;
-    }
-    return new Dimension(Math.max(width, getParent().getWidth()), height);
-  }
 }
